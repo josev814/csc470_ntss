2673aded733b585f3b16c2ede0be856b295eac7c
josev814 <josev814@gmail.com>
2023-10-14 11:28:16 -0400

    fixing pylint issues with constants

--- app/public_html/ntss/config/constants.py
+++ app/public_html/ntss/config/constants.py
@@ -1,3 +1,9 @@
+"""
+Define constants in this file that can be used across all of the classes
+
+DO NOT put sensitive information in here like usernames, passwords, hashes or secrets
+"""
+
 ALL_HTTP_METHODS = (
     'GET',
     'HEAD',
