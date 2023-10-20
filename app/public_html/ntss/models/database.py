0c9f483fcdee0df646692e40c47f5e4bfdb711f0
josev814 <josev814@gmail.com>
2023-10-20 01:00:28 -0400

    updating database and user models
    updating to use sqlalchemy
    fixing some pylint issues

--- app/public_html/ntss/models/database.py
+++ app/public_html/ntss/models/database.py
@@ -1,14 +1,49 @@
-class MysqlDatabase():
+"""
+Base package for accessing the database(s)
+"""
+from os import getenv
+from sqlalchemy import select, create_engine
+from sqlalchemy.orm import Session
+
+class MysqlDatabase:
+    """
+    The MySQL Datbase class
+    """
     table=None
 
-    def create(self):
-        pass
+    def __init__(self):
+        _engine = create_engine(
+            f"mysql://{getenv('db_user')}:{getenv('db_pass')}@{getenv('db_host')}:{getenv('db_port')}",
+            echo=True
+        )
+        _query = ''
+
+    def create(self, kwdict):
+        """
+        Method to create a record
+        """
+
+    def select(self, columns, filters=None):
+        """
+        Method to select data from the database
+        """
+        records = []
+        self._query = select(self.table.c['","'.join(columns)])
+        for filter in filters:
+            if filter.operator == '=':
+                self._query.where(f'{filter.column}' == f'{filter.value}')
     
-    def select(self, columns, filters):
-        pass
+        with Session(self._engine) as db_sess:
+            for row in db_sess.execute(self._query):
+                records.append(row)
+        return records
 
     def update(self):
-        pass
-    
+        """
+        Method to update a record in the database
+        """
+
     def delete(self):
-        pass
+        """
+        Method to delete a record in the database
+        """
--- app/public_html/ntss/models/user.py
+++ app/public_html/ntss/models/user.py
@@ -1,21 +1,54 @@
-from ntss.models.database import MysqlDatabase
+"""
+This package handles interactions with the database pertaining to users
+"""
+from argon2 import PasswordHasher
+from database import MysqlDatabase
 
 class Users(MysqlDatabase):
-    MysqlDatabase.table = 'users'
+    """
+    The Users class that interacts with the database
+    """
+
+    def __init__(self):
+        """
+        When initializing the Users model set the default table to users
+        """
+        super().__init__()
+        self.table = 'users'
 
     def get_permissions(self, user_id):
-        MysqlDatabase.table = 'user_permissions'
-        pass
-
-    def get_user(self, user_email:str, user_password: None) -> dict:
+        """
+        Gets permissions for a user
+        """
+        self.table = 'user_permissions'
         query_filter = [
-                {'column':'email', 'operator':'=', 'value': user_email}
+                {'column':'user_id', 'operator':'=', 'value': user_id}
             ]
-        self.select(
+        permissions = self.select(
             '*',
             query_filter
         )
-    
+        return permissions
+
+
+    def get_user(self, user_email:str, user_password: None) -> dict:
+        """
+        Retrieves a user from the database based on their user_email
+        """
+        query_filter = [
+                {'column':'email', 'operator':'=', 'value': user_email}
+            ]
+        records = self.select(
+            '*',
+            query_filter
+        )
+        user = []
+        for record in records:
+            if not user_password or \
+                (user_password and self._check_password(user_password, record['password'])):
+                user = record
+        return user
+
     def add_user(self, user_email:str, user_password: str) -> int:
         """
         Adds a user and returns their user_id from the database
@@ -26,19 +59,15 @@ class Users(MysqlDatabase):
                 'password': self._set_encrypted_password(user_password)
             }
         )
-    
+
     def _set_encrypted_password(self, password:str):
         """
         Encrypts the password for a user
         """
-        from argon2 import PasswordHasher
-        ph = PasswordHasher()
-        return ph.hash(password) # GOOD
+        return PasswordHasher().hash(password) # GOOD
 
     def _check_password(self, password:str, encrypted_password) -> bool:
         """
         Checks if the password submitted is the same as the password that was stored
         """
-        from argon2 import PasswordHasher
-        ph = PasswordHasher()
-        return ph.verify(encrypted_password, password) # GOOD
+        return PasswordHasher().verify(encrypted_password, password) # GOOD
