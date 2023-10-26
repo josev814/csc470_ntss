b5c11dd3afe841dfb0b01d94dcfe07108a7a4476
josev814 <josev814@gmail.com>
2023-10-26 16:17:46 -0400

    exclude tests path from pylin
    updating the session class to add docs and more methods

--- ntss_www/ntss/models/session.py
+++ ntss_www/ntss/models/session.py
@@ -6,8 +6,24 @@ from webob import cookies
 
 
 class session:
-
+    """
+    This class will do session management for users
+    """
     def add_session(self):
+        """
+        Method to add session information
+        """
         cookie_name = 'randomizedguid'
         cookie_value = json.dumps({'logged_in': 'true'})
         cookies.make_cookie(cookie_name, cookie_value)
+
+    def update_session(self):
+        """
+        Makes updates to the session
+        """
+
+    def delete_session(self, cookie_name):
+        """
+        Remove the session if it's expired
+        """
+        cookies.Cookie(cookie_name).clear()
