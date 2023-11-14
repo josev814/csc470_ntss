29f80bcd716c06c2e1e93e104c7800498d601a8f
Caileb Carter <ccarter10@broncos.uncfsu.edu>
2023-11-14 15:39:43 -0500

    Edit/Delete user functionality (#35)
    * Edit/Delete user functionality
    
    Created Edit User HTML page
    Defined delete_user in WSGI
    Defined edit_user method and delete_user method in Users Controller
    Defined edit_user method and delete_user method in Users Model
    Revised column setup in list_user HTML
    Added Is Active field to user preview in user_profile html
    Revised Error -> Notification on error_modal HTML
    Defined edit_user method in User View
    
    * adjusting javascript to route to the delete_user route
    
    * fixing pylint issues
    
    * def delete_user()
    
    Modified the delet_user method
    
    * removing print statement
    
    when calling __commit_query it's a function call, so needed ()
    
    * Folder and path modification
    
    added user html to user folder and modified paths in User View
    
    * User Controller Mod
    
    Deleted print statement ln 176
    
    * Edit_user form and method mod
    
    -User Model
    -User Controller
    -edit_user
    
    ---------
    
    Co-authored-by: josev814 <josev814@gmail.com>

--- ntss_www/ntss/models/user.py
+++ ntss_www/ntss/models/user.py
@@ -83,7 +83,7 @@ class Users(MysqlDatabase):
             limit=limit
         )
         return records
-    
+
     def get_user_roles(self, user_id):
         """
         Gets the assigned roles for the current user
@@ -91,7 +91,6 @@ class Users(MysqlDatabase):
         # TODO fix this
         print(f'Roles for {user_id}')
         return []
-        
 
     def add_user(self, user_email: str, user_password: str, *args) -> str|bool:
         """
@@ -125,7 +124,47 @@ class Users(MysqlDatabase):
         if self.db_create(user_values):
             return user_guid
         return False
-    
+
+    def edit_user(self, *args) -> bool:
+        """
+        Edits user in database
+        """
+        if len(args) == 1:
+            args = args[0]
+        else:
+            raise IndexError('Too many indexes in the Tuple')
+
+        user_values = {
+            'email': args.get('email'),
+            'prefix_name': args.get('prefix_name'),
+            'first_name': args.get('first_name'),
+            'middle_name': args.get('middle_name'),
+            'last_name': args.get('last_name'),
+            'suffix_name': args.get('suffix_name'),
+            'address': args.get('address'),
+            'address2': args.get('address2'),
+            'city': args.get('city'),
+            'state': args.get('state'),
+            'zip': args.get('zip'),
+            'phone': args.get('phone'),
+            'website': args.get('website'),
+            'user_roles': args.get('user_roles'),
+            'is_active': args.get('is_active')
+        }
+        if args.get('password') is not None:
+            user_values['password'] = self._set_encrypted_password(args.get('password'))
+
+        where_clause = [{'column': 'user_guid', 'operator': '=', 'value': args.get('user_guid')}]
+
+        return bool(self.db_update(user_values, where_clause))
+
+    def delete_user(self, user_guid):
+        """
+        Deletes the user from system
+        """
+        where_clause = [{'column': 'user_guid', 'operator': '=', 'value': user_guid}]
+        return bool(self.db_delete(where_clause))
+
     def add_auth_token(self, auth_token: str, user_id: int) -> str:
         """
         Sets the auth token for a user
