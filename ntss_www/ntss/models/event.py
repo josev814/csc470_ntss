55b49d9d96c06adbad9059502279b1d91123b96d
Caileb Carter <ccarter10@broncos.uncfsu.edu>
2023-11-15 18:54:14 -0500

    Pylint Update

--- ntss_www/ntss/models/event.py
+++ ntss_www/ntss/models/event.py
@@ -36,7 +36,8 @@ class Event(MysqlDatabase):
         records = self.get_event_by(guid=guid)
         return records[0]
 
-    def get_event_by(self, name=None, city=None, state=None, guid=None, user_guid = None, limit: int=1):
+    def get_event_by(self, name=None, 
+                     city=None, state=None, guid=None, user_guid = None, limit: int=1):
         """
         Performs a select query based on the params passed
         """
